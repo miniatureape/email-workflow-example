df010702a189f4fd29bc6ffe32e85a780ff0ca03
Justin <justin.donato@gmail.com>
2015-07-30 22:15:22 -0400

    Update make-email.js

--- make-email.js
+++ make-email.js
@@ -5,15 +5,6 @@ var juice = require('juice');
 var template = fs.readFileSync('basic.html', 'utf-8');
 var contents = fs.readFileSync('contents.txt', 'utf-8');
 
-var salutationRegExp = new RegExp(/Salutation:(.*)Lead:/m);
-var leadRegExp       = new RegExp(/Lead:(.*)Body:/m);
-var bodyRegExp       = new RegExp(/Body:(.*):/m);
-
-function getPart(regex, contents) {
-    console.log(contents.match(regex));
-    return contents.match(regex)[0];
-}
-
 function prepareData(contents) {
 
     var lines = contents.split('\n'),
